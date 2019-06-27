960602586da8f90490eb295764fed4c34a3add2e
cedavis12 <cedavis12@gmail.com>
2019-06-26 20:42:52 -0400

    1 file modified

--- components/Intro.js
+++ components/Intro.js
@@ -20,8 +20,8 @@ class Intro extends React.Component {
                             <h1 className="display-5 text-center">Safety Is Everybody's Business</h1>
                             <img src="https://advancedct.com/wp-content/uploads/2015/12/23.jpg" alt="safety first" className="img-fluid center" />
                             <hr/>
-                            <p className="lead">ObserveIt is a safety observation tool that allows you to quickly submit a Laboratory Safety Observation to your manager.</p>
-                            <p><strong>Help keep our laboratory safe by reinforcing safe work habits.</strong></p>
+                            <p className="lead text-center">ObserveIt is a safety observation tool that allows you to quickly submit a Laboratory Safety Observation to your manager.</p>
+                            <p className="text-center"><strong>Help keep our laboratory safe by reinforcing safe work habits.</strong></p>
                         </div>
                     </div>
                     <button type="submit" className="btn btn-outline-primary m-1" onClick={this.continue}>Let's Begin!</button>
