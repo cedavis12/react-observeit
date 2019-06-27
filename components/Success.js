0391c5d4b7813cdee85da4dd6149db3a4539b1c2
cedavis12 <cedavis12@gmail.com>
2019-06-26 20:51:53 -0400

    1 file modified

--- components/Success.js
+++ components/Success.js
@@ -4,8 +4,11 @@ const Success = () => {
     return (
   
     <div className="jumbotron jumbotron-fluid mt-4">
-      <h1 className="display-4 text-center">Thank you for your submission!</h1>
-      <p className="lead text-center">Your manager will be in touch with you to follow up.</p>
+      <div className="container">
+        <h1 className="text-center">Thank you for your submission!</h1>
+        <hr/>
+        <p className="lead text-center">Your manager will be in touch with you to follow up.</p>
+      </div>
     </div>
   
     )
