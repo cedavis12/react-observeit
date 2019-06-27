8103d812263186950a8771130399fe6f110b426d
cedavis12 <cedavis12@gmail.com>
2019-06-26 20:45:23 -0400

    update success component

--- components/Success.js
+++ components/Success.js
@@ -2,14 +2,12 @@ import React from 'react';
 
 const Success = () => {
     return (
-        <div className="container">
-            <div className="jumbotron jumbotron-fluid mt-4">
-                <div className="container">
-                    <h1 className="display-4">Thank you for your submission!</h1>
-                    <p className="lead">Your manager will be in touch with you to follow up.</p>
-                </div>
-            </div>
-        </div>
+  
+    <div className="jumbotron jumbotron-fluid mt-4">
+      <h1 className="display-4 text-center">Thank you for your submission!</h1>
+      <p className="lead text-center">Your manager will be in touch with you to follow up.</p>
+    </div>
+  
     )
 }
 
